eba03a7765659fad98b3310f77841ef7242ec9cb
Ananth <mvananthkumar@gmail.com>
2022-01-12 18:01:37 +0530

    Create succes-info-warning-error-exception.py

---
+++ succes-info-warning-error-exception.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 12 17:41:40 2022
+
+@author: Unbeknownstguy
+"""
+
+import streamlit as st
+
+# success
+st.success("Success")
+ 
+# success
+st.info("Information")
+ 
+# success
+st.warning("Warning")
+ 
+# success
+st.error("Error")
+
+
