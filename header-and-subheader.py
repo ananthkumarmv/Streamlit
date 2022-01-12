64cbfaf6e41faf1b27f839a8ddd53dd2bec58ca4
Ananth <mvananthkumar@gmail.com>
2022-01-12 18:01:28 +0530

    Create header-and-subheader.py

---
+++ header-and-subheader.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 12 17:35:28 2022
+
+@author: Unbeknownstguy
+"""
+
+import streamlit as st
+
+# Header
+st.header("This is a header")
+
+# Subheader
+st.subheader("This is a subheader")
