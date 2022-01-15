144dcfc453aadf54b0d5ad7356ec901a99a67c58
Ananth <mvananthkumar@gmail.com>
2022-01-15 20:50:12 +0530

    Create text-data.py

---
+++ text-data.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 15 20:39:36 2022
+
+@author: Unbeknownstguy
+"""
+
+import streamlit as st
+
+
+st.title('This is my first app!')
+st.header('This is a header')
+st.markdown('Streamlit is **_really_ cool**.')
+
