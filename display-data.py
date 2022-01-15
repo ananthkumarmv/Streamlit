a6814051ccad582aa40bf3873a263d28a3a9803f
Ananth <mvananthkumar@gmail.com>
2022-01-15 20:50:10 +0530

    Create display-data.py

---
+++ display-data.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 15 20:44:43 2022
+
+@author: Unbeknownstguy
+"""
+
+import streamlit as st
+import numpy as np
+import pandas as pd
+st.title('This is my first app!')
+st.write('This is a table')
+
+dataframe = pd.DataFrame(np.random.randn(10, 20),
+            columns = ('col %d' % i
+                       for i in range(20)))
+
+st.write(dataframe)
+
