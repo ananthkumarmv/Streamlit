313a6ef496887d9a2dc5b12771d3b5d3e95f5de7
Ananth <mvananthkumar@gmail.com>
2022-04-10 23:48:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from multiprocessing.sharedctypes import Value
+from operator import index
 import streamlit as st
 import pandas as pd
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
 header = st.container()
 dataset = st.container()
 features = st.container()
 model_training = st.container()
-
 
 
 with header:
@@ -44,9 +47,27 @@ with model_training:
 
     max_depth = sel_col.slider('What should be the max_depth of the model?', min_value=10, max_value=100, value=20, step=10)
 
-    n_estimtor = sel_col.selectbox('How many trees should there be?', options=[100, 200, 300])
+    n_estimator = sel_col.selectbox('How many trees should there be?', options=[100, 200, 300, 'No limit'], index = 0)
 
     input_feature = sel_col.text_input('Which feature should be used as the input feature?','PULocationID')
+
+    regr = RandomForestRegressor(max_depth = max_depth, n_estimators = n_estimator)
+
+    x = taxi_data[[input_feature]]
+    y = taxi_data[['trip_distance']]
+
+    regr.fit(x, y)
+    prediction = regr.predict(y)
+
+    disp_col.subheader('Mean absolute error of the model is:')
+    disp_col.write(mean_absolute_error(y, prediction))
+    disp_col.subheader('Mean squared error of the model is:')
+    disp_col.write(mean_squared_error(y, prediction))
+
+    disp_col.subheader('R squared error of the model is:')
+    disp_col.write(r2_score(y, prediction))
+
+
 
 
 
